503c3d9ff3011e257d7c00973c5121a104ae6248
Bas Roding <bas.roding@gmail.com>
2014-01-15 13:39:00 +0100

    Updated windows file structure

--- ggj2014.Windows/Program.cs
+++ ggj2014.Windows/Program.cs
@@ -18,7 +18,7 @@ namespace ggj2014
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+			using (var game = new Game1())
                 game.Run();
         }
     }
